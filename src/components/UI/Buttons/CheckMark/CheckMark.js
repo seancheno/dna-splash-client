d1771d2d073e1d00658fbbb5772d14d6a5bfd81d
Sean Cheno <sean.chenoweth@colorado.edu>
2019-07-23 11:57:53 -0600

    Updated styles

--- src/components/UI/Buttons/CheckMark/CheckMark.js
+++ src/components/UI/Buttons/CheckMark/CheckMark.js
@@ -6,13 +6,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const checkMark = props => (
 	<Link to={props.link}>
-		<div className={classes.Wrapper}>
-			<div className={classes.Container}>
-				<div className={classes.Text}>{props.children}</div>
-				<div className={classes.Arrow}>
-					<FontAwesomeIcon style={{ color: props.color }} icon={props.icon} />
-				</div>
-			</div>
+		<div className={classes.Button}>
+			<FontAwesomeIcon style={{ color: props.color }} icon={props.icon} />
+			<span style={{ marginLeft: "4px" }}>Continue</span>
 		</div>
 	</Link>
 );
